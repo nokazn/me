374976b85351767d3349ac86a7fb37ee26402106
nokazn <41154684+nokazn@users.noreply.github.com>
2024-03-10 23:57:18 +0900

    feat: update top page

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,8 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, button, div, p, text)
+import Html exposing (Html, a, button, div, h2, h3, li, main_, p, section, text, ul)
+import Html.Attributes exposing (href)
 
 
 
@@ -50,10 +51,113 @@ update _ model =
 
 view : Model -> Html Msg
 view model =
-    div []
+    let
+        username =
+            "nokazn"
+    in
+    main_ []
         [ button [] [ text "Hello" ]
         , button [] [ text "World" ]
         , p [] [ text model ]
+        , section []
+            [ h2 [] [ text "Whereabout" ]
+            , ul []
+                [ li []
+                    [ a
+                        [ href ("https://twitter.com/" ++ username) ]
+                        [ text "Twitter (X)" ]
+                    ]
+                , li []
+                    [ a
+                        [ href "https://bsky.app/profile/bsky.nokazn.me"
+                        ]
+                        [ text "Bluesky" ]
+                    ]
+                , li []
+                    [ a
+                        [ href "https://mstdn.jp/@nokazzn"
+                        ]
+                        [ text "Mastdon (mstdn.jp)" ]
+                    ]
+                , li []
+                    [ a
+                        [ href ("https://misskey.io/@" ++ username) ]
+                        [ text "Misskey (misskey.io)" ]
+                    ]
+                , li []
+                    [ a
+                        [ href "https://discord.com/users/642284424548843531" ]
+                        [ text "Discord" ]
+                    ]
+                , li []
+                    [ a
+                        [ href ("https://keybase.io/" ++ username) ]
+                        [ text "Keybase" ]
+                    ]
+                , li []
+                    [ a
+                        [ href ("https://www.reddit.com/user/" ++ username) ]
+                        [ text "Reddit" ]
+                    ]
+                , li []
+                    [ a
+                        [ href ("https://scrapbox.io/" ++ username) ]
+                        [ text "Scrapbox" ]
+                    ]
+                , li []
+                    [ a
+                        [ href "https://md.nokazn.me/" ]
+                        [ text "メモ帳" ]
+                    ]
+                , li []
+                    [ a
+                        [ href ("https://github.com/" ++ username) ]
+                        [ text "GitHub" ]
+                    ]
+                , li []
+                    [ a
+                        [ href ("https://gitlab.com/" ++ username) ]
+                        [ text "GitLab" ]
+                    ]
+                , li []
+                    [ a
+                        [ href ("https://stackoverflow.com/users/12688834/" ++ username) ]
+                        [ text "Stack Overflow" ]
+                    ]
+                , li []
+                    [ a
+                        [ href ("https://soundcloud.com/" ++ username) ]
+                        [ text "SoundCloud" ]
+                    ]
+                , li []
+                    [ a
+                        [ href ("https://bandcamp.com/" ++ username) ]
+                        [ text "Bandcamp" ]
+                    ]
+                , li []
+                    [ a
+                        [ href "https://open.spotify.com/user/baleariclemon" ]
+                        [ text "Spotify" ]
+                    ]
+                ]
+            ]
+        , section []
+            [ h2 [] [ text "Contact" ]
+            , ul []
+                [ li []
+                    [ text "Email: "
+                    , a
+                        [ href "mailto:me@nokazn.me" ]
+                        [ text "me@nokazn.me" ]
+                    ]
+                , li []
+                    [ text "Twitter: "
+                    , a
+                        [ href ("https://twitter.com" ++ username) ]
+                        [ text "@nokazn" ]
+                    ]
+                ]
+            ]
         ]
 
 

e6890cda4daae2594f59eb07d33baf7d86641365
nokazn <41154684+nokazn@users.noreply.github.com>
2024-03-16 21:37:31 +0900

    chore: update nix setups

--- flake.nix
+++ flake.nix
@@ -4,12 +4,16 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
   };
 
   outputs =
-    { self
-    , nixpkgs
+    { nixpkgs
     , flake-utils
+    , ...
     }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = nixpkgs.legacyPackages.${system};

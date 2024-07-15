39b77a8e4ce478e2ac9b683bedb68469e046652a
nokazn <41154684+nokazn@users.noreply.github.com>
2024-07-16 02:05:21 +0900

    chore: formatter settigns

--- justfile
+++ justfile
@@ -11,6 +11,26 @@ check:
   pnpm run check
   cargo check
 
+fmt check="":
+  if [[ "{{check}}" == "--check" ]]; then \
+    just fmt-rust --check; \
+    just fmt-nix --check; \
+    just fmt-dprint --diff; \
+  else \
+    just fmt-rust; \
+    just fmt-nix; \
+    just fmt-dprint; \
+  fi
+
+fmt-rust *flags:
+  cargo fmt {{flags}}
+
+fmt-nix *flags:
+  find . -type f -iname '*.nix' |  grep -v 'node_modules' | xargs nixpkgs-fmt {{flags}}
+
+fmt-dprint *flags:
+    dprint fmt {{flags}}
+
 fix:
   pnpm run fix
   cargo fix
--- packages/web/elm-srcs.nix
+++ packages/web/elm-srcs.nix
@@ -1,97 +1,97 @@
 {
 
-      "elm/browser" = {
-        sha256 = "0nagb9ajacxbbg985r4k9h0jadqpp0gp84nm94kcgbr5sf8i9x13";
-        version = "1.0.2";
-      };
+  "elm/browser" = {
+    sha256 = "0nagb9ajacxbbg985r4k9h0jadqpp0gp84nm94kcgbr5sf8i9x13";
+    version = "1.0.2";
+  };
 
-      "elm/core" = {
-        sha256 = "19w0iisdd66ywjayyga4kv2p1v9rxzqjaxhckp8ni6n8i0fb2dvf";
-        version = "1.0.5";
-      };
+  "elm/core" = {
+    sha256 = "19w0iisdd66ywjayyga4kv2p1v9rxzqjaxhckp8ni6n8i0fb2dvf";
+    version = "1.0.5";
+  };
 
-      "elm/html" = {
-        sha256 = "1n3gpzmpqqdsldys4ipgyl1zacn0kbpc3g4v3hdpiyfjlgh8bf3k";
-        version = "1.0.0";
-      };
+  "elm/html" = {
+    sha256 = "1n3gpzmpqqdsldys4ipgyl1zacn0kbpc3g4v3hdpiyfjlgh8bf3k";
+    version = "1.0.0";
+  };
 
-      "jfmengels/elm-review" = {
-        sha256 = "0s3kbmphy31yw3adkb9654bgr7lw4mylzb02pz7rsb67d7gpryrd";
-        version = "2.13.1";
-      };
+  "jfmengels/elm-review" = {
+    sha256 = "0s3kbmphy31yw3adkb9654bgr7lw4mylzb02pz7rsb67d7gpryrd";
+    version = "2.13.1";
+  };
 
-      "rtfeldman/elm-css" = {
-        sha256 = "1gwhgqwclc7clk1ns4qqzyn7b4wvcmccg9qavvb8m694qmwlkzjy";
-        version = "18.0.0";
-      };
+  "rtfeldman/elm-css" = {
+    sha256 = "1gwhgqwclc7clk1ns4qqzyn7b4wvcmccg9qavvb8m694qmwlkzjy";
+    version = "18.0.0";
+  };
 
-      "elm-explorations/test" = {
-        sha256 = "10k8ja78zrvkmjx96l99lmngk1d9ix0m1n2rai9x0nw47i4vx8gh";
-        version = "2.2.0";
-      };
+  "elm-explorations/test" = {
+    sha256 = "10k8ja78zrvkmjx96l99lmngk1d9ix0m1n2rai9x0nw47i4vx8gh";
+    version = "2.2.0";
+  };
 
-      "elm/bytes" = {
-        sha256 = "02ywbf52akvxclpxwj9n04jydajcbsbcbsnjs53yjc5lwck3abwj";
-        version = "1.0.8";
-      };
+  "elm/bytes" = {
+    sha256 = "02ywbf52akvxclpxwj9n04jydajcbsbcbsnjs53yjc5lwck3abwj";
+    version = "1.0.8";
+  };
 
-      "elm/json" = {
-        sha256 = "0kjwrz195z84kwywaxhhlnpl3p251qlbm5iz6byd6jky2crmyqyh";
-        version = "1.1.3";
-      };
+  "elm/json" = {
+    sha256 = "0kjwrz195z84kwywaxhhlnpl3p251qlbm5iz6byd6jky2crmyqyh";
+    version = "1.1.3";
+  };
 
-      "elm/parser" = {
-        sha256 = "0a3cxrvbm7mwg9ykynhp7vjid58zsw03r63qxipxp3z09qks7512";
-        version = "1.1.0";
-      };
+  "elm/parser" = {
+    sha256 = "0a3cxrvbm7mwg9ykynhp7vjid58zsw03r63qxipxp3z09qks7512";
+    version = "1.1.0";
+  };
 
-      "elm/project-metadata-utils" = {
-        sha256 = "0r5dvbipcc0jhwlpaxv9iw1vrmsk1yxnycm0k4lvw1rbgikis7rv";
-        version = "1.0.2";
-      };
+  "elm/project-metadata-utils" = {
+    sha256 = "0r5dvbipcc0jhwlpaxv9iw1vrmsk1yxnycm0k4lvw1rbgikis7rv";
+    version = "1.0.2";
+  };
 
-      "elm/random" = {
-        sha256 = "138n2455wdjwa657w6sjq18wx2r0k60ibpc4frhbqr50sncxrfdl";
-        version = "1.0.0";
-      };
+  "elm/random" = {
+    sha256 = "138n2455wdjwa657w6sjq18wx2r0k60ibpc4frhbqr50sncxrfdl";
+    version = "1.0.0";
+  };
 
-      "elm/time" = {
-        sha256 = "0vch7i86vn0x8b850w1p69vplll1bnbkp8s383z7pinyg94cm2z1";
-        version = "1.0.0";
-      };
+  "elm/time" = {
+    sha256 = "0vch7i86vn0x8b850w1p69vplll1bnbkp8s383z7pinyg94cm2z1";
+    version = "1.0.0";
+  };
 
-      "elm/url" = {
-        sha256 = "0av8x5syid40sgpl5vd7pry2rq0q4pga28b4yykn9gd9v12rs3l4";
-        version = "1.0.0";
-      };
+  "elm/url" = {
+    sha256 = "0av8x5syid40sgpl5vd7pry2rq0q4pga28b4yykn9gd9v12rs3l4";
+    version = "1.0.0";
+  };
 
-      "elm/virtual-dom" = {
-        sha256 = "1yvb8px2z62xd578ag2q0r5hd1vkz9y7dfkx05355iiy1d7jwq4v";
-        version = "1.0.3";
-      };
+  "elm/virtual-dom" = {
+    sha256 = "1yvb8px2z62xd578ag2q0r5hd1vkz9y7dfkx05355iiy1d7jwq4v";
+    version = "1.0.3";
+  };
 
-      "miniBill/elm-unicode" = {
-        sha256 = "07hq9q9chw4fh3ypzns5njxgn92m0qslriaawadbh9dmc22xijd9";
-        version = "1.1.0";
-      };
+  "miniBill/elm-unicode" = {
+    sha256 = "07hq9q9chw4fh3ypzns5njxgn92m0qslriaawadbh9dmc22xijd9";
+    version = "1.1.0";
+  };
 
-      "robinheghan/murmur3" = {
-        sha256 = "15asmgr2zqh7rkywrg5647rpdqkpzxk02v5qc6ndj60jza3gsmjk";
-        version = "1.0.0";
-      };
+  "robinheghan/murmur3" = {
+    sha256 = "15asmgr2zqh7rkywrg5647rpdqkpzxk02v5qc6ndj60jza3gsmjk";
+    version = "1.0.0";
+  };
 
-      "rtfeldman/elm-hex" = {
-        sha256 = "1y0aa16asvwdqmgbskh5iba6psp43lkcjjw9mgzj3gsrg33lp00d";
-        version = "1.0.0";
-      };
+  "rtfeldman/elm-hex" = {
+    sha256 = "1y0aa16asvwdqmgbskh5iba6psp43lkcjjw9mgzj3gsrg33lp00d";
+    version = "1.0.0";
+  };
 
-      "stil4m/elm-syntax" = {
-        sha256 = "17c5hz6ar1m9swz76c9v8vf2ymkscbpal8s63nszcbk9wjzlmkv9";
-        version = "7.3.2";
-      };
+  "stil4m/elm-syntax" = {
+    sha256 = "17c5hz6ar1m9swz76c9v8vf2ymkscbpal8s63nszcbk9wjzlmkv9";
+    version = "7.3.2";
+  };
 
-      "stil4m/structured-writer" = {
-        sha256 = "02k32yaw275bivab90wy8qkbys3gg4fw53f798dzf1j9wharhg12";
-        version = "1.0.3";
-      };
+  "stil4m/structured-writer" = {
+    sha256 = "02k32yaw275bivab90wy8qkbys3gg4fw53f798dzf1j9wharhg12";
+    version = "1.0.3";
+  };
 }
